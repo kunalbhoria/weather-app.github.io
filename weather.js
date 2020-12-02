5f78edfdead6abc3076526b464f316a1f5e3ef1b
kunalbhoria <75310757+kunalbhoria@users.noreply.github.com>
2020-12-02 11:14:13 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@
 
 const weatherApi ={
     key:"c60e21be15362fabae87b660d2dad3f4" ,
-    address:"http://api.openweathermap.org/data/2.5/weather"
+    address:"https://api.openweathermap.org/data/2.5/weather"
 }
 
 //getname
